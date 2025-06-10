cea7bb3ae2c4e7a0479fba96e24471c207c82544
Bera0 <force1321@naver.com>
2025-06-10 17:11:42 +0900

    Add sys_report.sh with memory and disk info

--- 2025-06-09_sys-report/sys_report.sh
+++ 2025-06-09_sys-report/sys_report.sh
@@ -8,4 +8,21 @@ echo "🖥 호스트명: $(hostname)" >> $REPORT_FILE
 echo "👤 현재 사용자: $(whoami)" >> $REPORT_FILE
 echo "⏱ 시스템 업타임: $(uptime)" >> $REPORT_FILE
 
+echo "메모리 정보 "
+free -h
+echo ""
+
+echo "디스크 사용량"
+df -h
+echo ""
+
+echo "시스템 가동 정보"
+uptime
+echo ""
+
+echo "로그인 사용자"
+who
+
+
 echo "✅ 리포트가 저장되었습니다: $REPORT_FILE"
+
